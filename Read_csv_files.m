9ffcdaf396456a1a2760a08371930392eb829178
Saad Abdullah <aisaad@hotmail.com>
2023-09-01 16:42:21 +0200

    Update Read_csv_files.m

--- Read_csv_files.m
+++ Read_csv_files.m
@@ -12,13 +12,7 @@ csvFiles = dir(fullfile(folder, '*.csv'))
 % Initialize a cell array to store the data from each CSV file
 data = cell(1, numel(csvFiles));
 
-no_of_files = size(data);
-no_of_files = no_of_files(1,2);
-no_of_rows = size(data{1});
-no_of_rows = no_of_rows(1,1);
 
-y = zeros(no_of_rows,no_of_files);
-x = zeros(no_of_rows,1);
 % Loop through each CSV file and read its contents
 for i = 1:numel(csvFiles)
     % Get the file name
